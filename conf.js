87be60d8a02c4644509f2750619ffac86f12d0bd
Jovan Zejnula <jovanzeka97@gmail.com>
2020-11-30 15:43:21 +0100

    Protractor tutorial

---
+++ conf.js
@@ -0,0 +1,5 @@
+exports.config = {
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['step0/spec.js','step1/spec.js','step2/spec.js','step3/spec.js','step4/spec.js']
+  } 
