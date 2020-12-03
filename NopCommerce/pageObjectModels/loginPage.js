50e36078dfeb20089d779bab00f0d87a16a670bf
Jovan Zejnula <jovanzeka97@gmail.com>
2020-12-03 13:51:32 +0100

    PageObjectModel fix

--- NopCommerce/pageObjectModels/loginPage.js
+++ NopCommerce/pageObjectModels/loginPage.js
@@ -4,18 +4,18 @@ let LoginPage = function(){
     this.loginLink = element(by.css('a[class="ico-login"]'));
     this.errorMsg = element(by.css('div[class="message-error validation-summary-errors"] ul li'));
     this.emptyEmailErrorMsg = element(by.css('span[id="Email-error"]'))
+    let usernameInput =  element(by.css('input[class="email"]'));
+    let passwordInput = element(by.css('input[class="password"]'));
+    let loginButton = element(by.css('input[class="button-1 login-button"]'));
 
 
     this.testSignIn = function(username,password){
         let EC = protractor.ExpectedConditions;
       
-        browser.wait(EC.presenceOf(element(by.css('input[class="email"]')),5000))
-        let usernameInput =  element(by.css('input[class="email"]'));
-        let passwordInput = element(by.css('input[class="password"]'));
+        browser.wait(EC.presenceOf(usernameInput,5000))
         usernameInput.sendKeys(username);
         passwordInput.sendKeys(password);
         
-        let loginButton = element(by.css('input[class="button-1 login-button"]'));
         loginButton.click();
     }
 }
